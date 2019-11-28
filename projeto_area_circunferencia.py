46c7a6d58e8a313ee6e81db7f10e0bc882a7a986
RobbFreitas <robertocompromissos@gmail.com>
2019-11-28 18:03:31 -0300

    usando math

--- projeto_area_circunferencia.py
+++ projeto_area_circunferencia.py
@@ -1,5 +1,5 @@
 #Fórmula inicial área da circunferência
-pi = 3.14
+from math import pi
 raio = float(input('Informe o valor do raio: '))
 calculo = pi * (raio **2 )
 print(f'O valor da área é {calculo}m')
