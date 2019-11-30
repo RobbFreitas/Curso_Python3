67507217aa2f77ff6a64c5a0bbabee1bd4465edd
RobbFreitas <robertocompromissos@gmail.com>
2019-11-30 10:15:38 -0300

    up

--- projeto_area_circunferencia.py
+++ projeto_area_circunferencia.py
@@ -1,16 +1,15 @@
-#Fórmula inicial área da circunferência
+# Fórmula inicial área da circunferência
 #!/user/local/bin/python3
 from math import pi
 
 
-def circulo (raio):
-  return pi * float(raio )**2 
+def circulo(raio):
+    return pi * float(raio)**2
 
 
 if __name__ == '__main__':
-  raio = float(input('Informe o valor do raio:  '))
-  if raio < 2:
-    print('É necessário informar o raio do círculo\nSintaxe: area_circulo <raio>')
-  else:
-    area = circulo(raio)
-    print(f'A área do círculo é {area}')
+    raio = float(input('Informe o valor do raio:  '))
+    if raio < 2 or raio == 0:
+        print('É necessário informar o raio do círculo\nSintaxe: {} <raio>'.format(raio))
+    else:
+        area = circulo(raio)
