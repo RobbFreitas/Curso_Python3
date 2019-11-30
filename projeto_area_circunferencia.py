b7dd4c7642056f7de4fb1b75faac2b337a5544ee
RobbFreitas <robertocompromissos@gmail.com>
2019-11-30 11:01:50 -0300

    up

--- projeto_area_circunferencia.py
+++ projeto_area_circunferencia.py
@@ -7,9 +7,14 @@ def circulo(raio):
     return pi * float(raio)**2
 
 
+def help ():
+   print('É necessário informar o raio do círculo\nSintaxe: {} <raio>'.format(raio))
+
+
 if __name__ == '__main__':
     raio = float(input('Informe o valor do raio:  '))
     if raio < 2 or raio == 0:
-        print('É necessário informar o raio do círculo\nSintaxe: {} <raio>'.format(raio))
+      help()
     else:
-        area = circulo(raio)
+      area = circulo(raio)
+      print('O tamanho da área do círculo é {:.2f}m'.format(area))
