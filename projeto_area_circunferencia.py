749c7ef64a428a2b44a7a610bfd089f8da35e377
RobbFreitas <robertocompromissos@gmail.com>
2019-11-29 22:45:11 -0300

    up

--- projeto_area_circunferencia.py
+++ projeto_area_circunferencia.py
@@ -1,7 +1,7 @@
 #Fórmula inicial área da circunferência
+#!/user/local/bin/python3
 from math import pi
-raio = float(input('Informe o valor do raio: '))
-calculo = pi * (raio **2 )
+if __name__ == '__main__':
+  raio = float(input('Informe o valor do raio: '))
+  calculo = pi * (raio **2 )
 print(f'O valor da área é {calculo}m')
-
-print('Nome do módulo', __name__)
