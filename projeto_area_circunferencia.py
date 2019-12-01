98d20270d99ea7552c372932fdc14caca36d92e7
RobbFreitas <robertocompromissos@gmail.com>
2019-12-01 09:56:50 -0300

    up

--- projeto_area_circunferencia.py
+++ projeto_area_circunferencia.py
@@ -1,18 +1,26 @@
 # Fórmula inicial área da circunferência
 #!/user/local/bin/python3
 from math import pi
+import sys 
+
+
+class TerminalColor:
+  ERRO = '\033[91m'
+  NORMAL = '\033[0m'
 
 
 def circulo(raio):
     return pi * float(raio)**2
 
 
-def help ():
-   print('É necessário informar o raio do círculo\nSintaxe: {} <raio>'.format(raio))
+def help():
+   print(TerminalColor.ERRO + 
+            f'É necessário informar o raio do círculo Sintaxe: {raio} <raio>'
+            + TerminalColor.NORMAL)
 
 
 if __name__ == '__main__':
-    raio = float(input('Informe o valor do raio:  '))
+    raio = float(input('Informe o valor do raio (apenas valor numérico):  '))
     if raio < 2 or raio == 0:
       help()
     else:
